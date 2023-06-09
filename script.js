b6acb2121a6fdc0ad6e5b2554a7b6e7c8dba2048
Dayne Yarbrough <myhoody@outlook.com>
2023-06-09 09:14:35 -0700

    Initialize gh-pages

---
+++ script.js
@@ -0,0 +1,184 @@
+const DEFAULT_PORT = 22023;
+
+const IP_REGEX = /(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}/;
+
+async function parseAddressAsync(serverAddress) {
+    if (serverAddress === "localhost") {
+        serverAddress = "127.0.0.1";
+    }
+
+    if (IP_REGEX.test(serverAddress)) {
+        // TODO: wait for update fixing StaticRegionInfo
+        return [serverAddress, serverAddress];
+    }
+
+    /** @type {{ Status: number, Answer: { type: number, data: string }[] }} */
+    const dns = await (await fetch("https://cloudflare-dns.com/dns-query?type=A&name=" + serverAddress, {
+        headers: {
+            "Accept": "application/dns-json"
+        }
+    })).json();
+
+    if (dns && dns.Status === 0) {
+        for (const record of dns.Answer) {
+            if (record.type === 1 && IP_REGEX.test(record.data)) {
+                return [record.data, serverAddress];
+            }
+        }
+    }
+
+    const message = "Failed DNS request for " + serverAddress;
+
+    alert(message);
+    throw Error(message);
+}
+
+function parseForm() {
+    const serverAddress = document.getElementById("address").value.trim();
+    const serverPort = parseInt(document.getElementById("port").value) ?? DEFAULT_PORT;
+    const serverName = document.getElementById("name").value || "Impostor";
+
+    return [serverAddress, serverPort, serverName];
+}
+
+async function downloadAsync() {
+    const [serverAddress, serverPort, serverName] = parseForm();
+    const [serverIp, serverFqdn] = await parseAddressAsync(serverAddress);
+
+    const json = generateRegionInfo(
+        serverName,
+        serverIp,
+        serverFqdn,
+        serverPort
+    );
+    const blob = new Blob([json], { type: "text/plain" });
+    saveFile(blob, "regionInfo.json");
+
+    return false;
+}
+
+async function openApp() {
+    const [serveraddress, serverport, servername] = parseForm();
+    const [serverip, _] = await parseAddressAsync(serveraddress);
+
+    const params = new URLSearchParams({
+        servername,
+        serverport,
+        serverip,
+    });
+    const url = `amongus://init?${params.toString()}`;
+    window.location = url;
+
+    return false;
+}
+
+let currentPlatform;
+
+function setEnabled(platform, value) {
+    for (const e of document.querySelectorAll(`.${platform}-support`)) {
+        e.style.display = value ? "block" : "none";
+    }
+}
+
+function setPlatform(platform) {
+    if (currentPlatform === platform) {
+        return;
+    }
+
+    if (currentPlatform) {
+        setEnabled(currentPlatform, false);
+        document.getElementById(currentPlatform).classList.remove("text-primary");
+    }
+
+    setEnabled(platform, true);
+    document.getElementById(platform).classList.add("text-primary");
+
+    currentPlatform = platform;
+}
+
+function generateRegionInfo(name, ip, fqdn, port) {
+    const regions = [
+        // Add default regions so they also show up in the menu
+        {
+            $type: "DnsRegionInfo, Assembly-CSharp",
+            Fqdn: "na.mm.among.us",
+            DefaultIp: "50.116.1.42",
+            Port: 22023,
+            Name: "North America",
+            TranslateName: 289,
+            UseDtls: true,
+        },
+        {
+            $type: "DnsRegionInfo, Assembly-CSharp",
+            Fqdn: "eu.mm.among.us",
+            DefaultIp: "172.105.251.170",
+            Port: 22023,
+            Name: "Europe",
+            TranslateName: 290,
+            UseDtls: true,
+        },
+        {
+            $type: "DnsRegionInfo, Assembly-CSharp",
+            Fqdn: "as.mm.among.us",
+            DefaultIp: "139.162.111.196",
+            Port: 22023,
+            Name: "Asia",
+            TranslateName: 291,
+            UseDtls: true,
+        },
+        // Followed by the custom region
+        {
+            $type: "DnsRegionInfo, Assembly-CSharp",
+            Fqdn: fqdn,
+            DefaultIp: ip,
+            Port: port,
+            Name: name,
+            TranslateName: 1003, // StringNames.NoTranslation
+            UseDtls: false, // As no custom key can be specified, we need to disable DTLS on custom servers.
+        },
+    ];
+
+    const jsonServerData = {
+        CurrentRegionIdx: 3,
+        Regions: regions,
+    };
+
+    return JSON.stringify(jsonServerData);
+}
+
+function saveFile(blob, fileName) {
+    let url = URL.createObjectURL(blob);
+
+    let a = document.createElement("a");
+    document.body.appendChild(a);
+    a.style.display = "none";
+    a.href = url;
+    a.download = fileName;
+    a.click();
+
+    URL.revokeObjectURL(url);
+}
+
+function fillFromLocationHash() {
+    const urlServerAddress = document.location.hash.substr(1).split(":");
+    const serverAddress = urlServerAddress[0];
+    const serverPort = urlServerAddress.length > 1 ? urlServerAddress[1] : DEFAULT_PORT.toString();
+
+    if (serverAddress) {
+        document.getElementById("address").value = serverAddress;
+    }
+
+    if (new RegExp("^[0-9]+$", "g").test(serverPort)) {
+        document.getElementById("port").value = serverPort;
+    }
+}
+
+fillFromLocationHash();
+
+if (["iPhone", "iPad", "iPod"].indexOf(window.navigator.platform) !== -1) {
+    setPlatform("ios");
+} else if (/Android/.test(window.navigator.userAgent)) {
+    setPlatform("android");
+} else {
+    setPlatform("desktop");
+}
